5ee3e8fe63e515625401012a5d084508079ee850
grlbrwrg <walter.rothenberger@googlemail.com>
2016-01-15 15:16:28 +0100

    Updated plotting functions
    Now using matplotlib to internally generate plots!

--- hilde/views.py
+++ hilde/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
-from datetime import datetime
-from chartit import DataPool, Chart
+import datetime
 from models import Walter
+
 
 # Create your views here.
 
@@ -10,38 +10,43 @@ def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
 
 def tagesfarbe():
-    zeit = datetime.now()
+    zeit = datetime.datetime.now()
     r = zeit.second*3
     g = 100+zeit.minute*2
     b = zeit.hour*5
     rgb = r,g,b
     return rgb
 
+def simple(request):
+    import random
+    import django
+    import datetime
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure(figsize=(5,5), dpi=80)
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    response=django.http.HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
 
 def index(request):
-    Grafik = DataPool(
-        series=
-        [{'options': {
-            'source': Walter.objects.all()},
-            'terms':[
-                'x',
-                'y']}
-            ])
-    chart = Chart(
-        datasource = Grafik,
-        series_options =
-            [{'options':{
-                'type': 'line',
-                'stacking': False},
-                'terms':{
-                  'x':['y']
-                }}],
-        chart_options =
-            {'title': {
-                   'text': 'Testgrafik'},
-               })
 
-    return render(request,'hilde/index.html',{'farbe':rgb_to_hex(tagesfarbe()),'chart':chart})
+    return render(request,'hilde/index.html',{'farbe':rgb_to_hex(tagesfarbe())})
     
 def einloggen(request):
     username = request.POST['username']
@@ -53,3 +58,15 @@ def einloggen(request):
             return HttpResponseRedirect('/admin/')
         return HttpResponse('Fehler - Account inaktiv')
     return HttpResponse('Fehler - falscher Username oder falsches Passwort')
+    
+def hildeAPI(request):
+    if request.GET:
+        data = request.GET
+        key = data.get('k')
+        if int(key) == 1:
+            
+            
+            return HttpResponse('LALALA')
+        return HttpResponse(key)
+    else:
+        return HttpResponseRedirect('/')
